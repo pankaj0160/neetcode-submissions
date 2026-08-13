5b148158cb6fedf65f90b01561f6875db483e172
Pankaj Thakur <pankajthakur.dev01@gmail.com>
2026-08-13 07:54:20 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int prev = prices[0];
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < prev)
+                prev = prices[i];
+            else
+                ans = max(ans, (prices[i] - prev));
+        }
+        return ans;
+    }
+};
