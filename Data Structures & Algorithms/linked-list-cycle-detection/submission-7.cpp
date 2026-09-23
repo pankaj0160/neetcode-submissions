efe3db461c19760749a5f7732fbe0f657ef069e4
Pankaj Thakur <pankajthakur.dev01@gmail.com>
2026-09-23 11:27:04 +0530

    Add: linked-list-cycle-detection - submission-7

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-7.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+/**
+ * Definition for singly-linked list.
+  * struct ListNode {
+   *     int val;
+    *     ListNode *next;
+     *     ListNode() : val(0), next(nullptr) {}
+      *     ListNode(int x) : val(x), next(nullptr) {}
+       *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+        * };
+         */
+
+         class Solution {
+            public:
+                bool hasCycle(ListNode* head) {
+                        // fast and slow pointer approach
+                                // moving fast to 2 step at a time and slow to 1 step at a time
+
+                                        ListNode* slow = head;
+                                                ListNode* fast = head;
+
+                                                        if (!head) return false;
+
+                                                                while (fast && fast->next) {
+                                                                            fast = fast->next->next;
+                                                                                        slow = slow->next;
+                                                                                                    if (fast == slow) return true;
+                                                                                                            }
+                                                                                                                    return false;
+                                                                                                                        }
+                                                                                                                        };
+
